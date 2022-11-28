9d68fb469d3291686b2d711a95c6b5744ccc0a68
ShinkarenkoMaxim <shinkarenko.max97@gmail.com>
2022-11-28 22:01:08 +0500

    feat: add frontend serving

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: `${__dirname}/../.env` });
 
 import express, { Request, Response } from 'express';
-import routes from './routes';
+import apiRoutes from './routes';
 
 const app = express();
 
-app.use('/api', routes);
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server has been started at ${process.env.PORT}`);
